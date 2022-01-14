5036bfcef7e12a631b314d30c6d75c8674d09f73
alan <alan_chen1@brown.edu>
2022-01-14 17:14:56 -0500

    reformatting

--- templates/cptemplate.cpp
+++ templates/cptemplate.cpp
@@ -1,5 +1,5 @@
 #include <bits/stdc++.h>
-
+#define DEBUG
 using namespace std;
 
 typedef long long ll;
@@ -12,12 +12,36 @@ typedef long long ll;
 
 template<typename T> bool ckmin(T& a, const T& b) { return b < a ? a = b, true : false; }
 template<typename T> bool ckmax(T& a, const T& b) { return b > a ? a = b, true : false; }
-template<typename T> ostream& operator<<(ostream &cout, vector<T> const &v) {
-	cout << "["; for (int i = 0; i < (int)v.size(); i++) {if (i) cout << ", "; cout << v[i]; } return cout << "]";
-}
+
+void __print(int x) {cerr << x;}
+void __print(long x) {cerr << x;}
+void __print(long long x) {cerr << x;}
+void __print(unsigned x) {cerr << x;}
+void __print(unsigned long x) {cerr << x;}
+void __print(unsigned long long x) {cerr << x;}
+void __print(float x) {cerr << x;}
+void __print(double x) {cerr << x;}
+void __print(long double x) {cerr << x;}
+void __print(char x) {cerr << '\'' << x << '\'';}
+void __print(const char *x) {cerr << '\"' << x << '\"';}
+void __print(const string &x) {cerr << '\"' << x << '\"';}
+void __print(bool x) {cerr << (x ? "true" : "false");}
+ 
+template<typename T, typename V>
+void __print(const pair<T, V> &x) {cerr << '{'; __print(x.first); cerr << ", "; __print(x.second); cerr << '}';}
+template<typename T>
+void __print(const T &x) {int f = 0; cerr << '{'; for (auto &i: x) cerr << (f++ ? ", " : ""), __print(i); cerr << "}";}
+void _print() {cerr << "]\n";}
+template <typename T, typename... V>
+void _print(T t, V... v) {__print(t); if (sizeof...(v)) cerr << ", "; _print(v...);}
+#ifdef DEBUG
+#define dbg(x...) cerr << "\e[91m"<<__func__<<":"<<__LINE__<<" [" << #x << "] = ["; _print(x); cerr << "\e[39m" << endl;
+#else
+#define dbg(x...)
+#endif
 
 void solve(){
-	
+    
 }
 
 int main(){
